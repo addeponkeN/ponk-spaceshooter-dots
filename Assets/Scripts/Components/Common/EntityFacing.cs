fe90c8735cb23790cc07f8f7f6c2ee6faa921713
addeponkeN <addeponken@hotmail.com>
2023-10-30 19:40:19 +0100

    Added some common components

---
+++ Assets/Scripts/Components/Common/EntityFacing.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+public struct EntityFacing : IComponentData
+{
+    public float Value;
+}
---
+++ Assets/Scripts/Components/Common/EntityTimer.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+public struct EntityTimer : IComponentData
+{
+    public float Value;
+}
